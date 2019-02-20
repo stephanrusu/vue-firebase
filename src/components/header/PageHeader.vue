26651888f41a102964a3dacb70dd180d25583ac3
Stefan Rusu <sru@miracle.dk>
2019-02-20 10:29:13 +0200

    breadcrumbs and utilities

--- src/components/header/PageHeader.vue
+++ src/components/header/PageHeader.vue
@@ -1,19 +1,27 @@
 <template>
-  <section class="hero is-info">
-    <div class="hero-body has-text-centered">
-      <p class="title">
-        {{ title | capitalize }}
-      </p>
-      <p class="subtitle" v-show="results > 0">
-        {{ $t("header.results", {total: this.results}) }}
-      </p>
-    </div>
-  </section>
+  <div>
+    <section class="hero is-info">
+      <div class="hero-body has-text-centered">
+        <p class="title">
+          {{ title | capitalize }}
+        </p>
+        <p class="subtitle" v-show="results > 0">
+          {{ $t("header.results", {total: this.results}) }}
+        </p>
+      </div>
+    </section>
+    <breadcrumbs />
+  </div>
 </template>
 
 <script>
+import Breadcrumbs from '@/views/common/Breadcrumbs.vue';
+
 export default {
   name: 'PageHeader',
+  components: {
+    Breadcrumbs,
+  },
   props: {
     title: {
       type: String,
@@ -26,6 +34,7 @@ export default {
   },
 };
 </script>
+
 <style lang="sass">
 .hero
   margin: 1.5rem 0
---
+++ src/views/common/Breadcrumbs.vue
@@ -0,0 +1,18 @@
+<template>
+   <nav key="breadcrumbs" class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <router-link tag="li" :to="{ name: 'home' }">
+        <a>Home</a>
+      </router-link>
+      <router-link tag="li" :to="{ name: $route.name }" class="is-active">
+        <a>{{ $route.name }}</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumbs',
+};
+</script>
